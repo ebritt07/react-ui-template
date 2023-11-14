e23bffbf3b279d59648ad72febb47abc5c0370f7
ebritt07 <44182357+ebritt07@users.noreply.github.com>
2023-11-14 15:23:02 -0600

    start a clean vite project

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Table from './components/table/Table.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,25 +10,39 @@ function App() {
   return (
     <>
       <div>
+      {/*
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        */}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <h1>React Template</h1>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <p>visit <a href="https://www.google.com" target="_blank">google</a></p>
+      <ul>
+      <li>list one</li>
+      <li>list one</li>
+      <li>list one</li>
+      </ul>
+
+      <Table/>
+
+
+      <p>This UI's url: <a href="http://localhost:3000">http://localhost:3000</a></p>
+      <p>Backend url: <a href="http://localhost:4000">http://localhost:4000</a></p>
+
     </>
   )
 }
---
+++ src/components/table/Table.tsx
@@ -0,0 +1,32 @@
+import './Table.css'
+
+
+
+const Table = () => {
+return (
+    <>
+    <table>
+          <caption>template table</caption>
+          <tbody>
+              <tr>
+                <th>col 1</th><th>col 2</th><th>col 3</th>
+              </tr>
+              <tr>
+                <td>her</td><td>her</td><td>her</td>
+                </tr>
+              <tr>
+                <td>her</td><td>her</td>
+                </tr>
+              <tr>
+                <td>sdf</td><td>sdf</td><td>sdf</td>
+                </tr>
+              <tr>
+                <td>sdf</td><td>sdf</td>
+                </tr>
+            </tbody>
+    </table>
+    </>
+    );
+}
+
+export default Table;
